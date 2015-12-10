ed6c0ea0ae0086e2fbc2a9c40cd43882db7c8306
Thomas Bennett <tbenz9@gmail.com>
2015-12-09 21:56:49 -0700

    cleaned up the code and created sections

--- eyes/go/eyesd.go
+++ eyes/go/eyesd.go
@@ -9,28 +9,27 @@ import (
     "os"
 )
 
-var localDatabasePath string = "/tmp/eyes/eyesd.db"
-var debug bool = false
 
-func changedState(state int) {
-    insertStateIntoDatabase(state)
-    if debug {fmt.Printf("Changed State to %v\n", state)}
-}
+
+/////////////////////////////////////////////////////////////
+//
+// Database Functions and Variables
+//
+/////////////////////////////////////////////////////////////
+
+var localDatabasePath string = "/tmp/eyes/eyesd.db"
 
 func insertStateIntoDatabase(state int) int64 {
     db, err := sql.Open("sqlite3", localDatabasePath)
     checkErr(err)
  
-    // insert
     stmt, err := db.Prepare("INSERT INTO state (state) values(?)")
     checkErr(err)
-
     res, err := stmt.Exec(state)
     checkErr(err)
 
     id, err := res.LastInsertId()
     checkErr(err)
-
     if debug {fmt.Printf("Database returned ID %v", id)}
     db.Close()
     return id
@@ -43,8 +42,33 @@ func setupLocalDatabase() {
 
     _, err = db.Exec("CREATE TABLE IF NOT EXISTS state (metricsID INTEGER PRIMARY KEY, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, state INTEGER NOT NULL)")
     checkErr(err)
-    
     db.Close()
+}
+
+/////////////////////////////////////////////////////////////
+//
+// Websocket Functions
+//
+/////////////////////////////////////////////////////////////
+
+var remoteServerAddress string = "192.168.0.106"
+
+func setupWebsocket() {
+    if debug{fmt.Println("Attempting to establish a websocket")}
+    if debug{fmt.Printf("Remote Server = %v",remoteServerAddress)}
+}
+
+/////////////////////////////////////////////////////////////
+//
+// Other Functions and Variables
+//
+/////////////////////////////////////////////////////////////
+
+var debug bool = false
+
+func changedState(state int) {
+    insertStateIntoDatabase(state)
+    if debug {fmt.Printf("TV Changed State to %v\n", state)}
 }
 
 func checkErr(err error) {
@@ -53,12 +77,19 @@ func checkErr(err error) {
     }
 }
 
+/////////////////////////////////////////////////////////////
+//
+// Main
+//
+/////////////////////////////////////////////////////////////
+
 func main() {
 
     cec.Open("", "cec.go")
     var previousState int = 5
 
     // Initial Startup tasks
+    setupWebsocket()
     setupLocalDatabase()
 
     // Check the TV state every second
