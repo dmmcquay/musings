f73890e1ea0949367839b0bab0553a6b13864304
Thomas Bennett <tbenz9@gmail.com>
2015-12-09 21:06:59 -0700

    got polling working, and writing to the db.  Also included debug flag if needed

--- eyes/go/eyesd.go
+++ eyes/go/eyesd.go
@@ -3,36 +3,60 @@ package main
 import (
     "database/sql"
     _ "github.com/mattn/go-sqlite3"
-    "github.com/chbmuc/cec"
+    "github.com/tbenz9/cec"
     "time"
     "fmt"
-    "log"
     "os"
 )
 
-func changedState(state string) {
-    fmt.Print(state)
+var localDatabasePath string = "/tmp/eyes/eyesd.db"
+var debug bool = false
+
+func changedState(state int) {
+    insertStateIntoDatabase(state)
+    if debug {fmt.Printf("Changed State to %v\n", state)}
+}
+
+func insertStateIntoDatabase(state int) int64 {
+    db, err := sql.Open("sqlite3", localDatabasePath)
+    checkErr(err)
+ 
+    // insert
+    stmt, err := db.Prepare("INSERT INTO state (state) values(?)")
+    checkErr(err)
+
+    res, err := stmt.Exec(state)
+    checkErr(err)
+
+    id, err := res.LastInsertId()
+    checkErr(err)
+
+    if debug {fmt.Printf("Database returned ID %v", id)}
+    db.Close()
+    return id
 }
 
 func setupLocalDatabase() {
-    err := os.MkdirAll("/opt/eyes",0755)
-    db, err := sql.Open("sqlite3", "/opt/eyes/eyes.db")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+    err := os.MkdirAll("/tmp/eyes",0755)
+    db, err := sql.Open("sqlite3", localDatabasePath)
+    checkErr(err)
 
-    _, err = db.Exec("CREATE TABLE IF NOT EXISTS state (metricsID INTEGER PRIMARY KEY, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, state BOOLEAN NOT NULL)")
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS state (metricsID INTEGER PRIMARY KEY, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, state INTEGER NOT NULL)")
+    checkErr(err)
+    
+    db.Close()
+}
+
+func checkErr(err error) {
     if err != nil {
-        log.Printf("%q\n", err)
-        return
+        panic(err)
     }
 }
 
 func main() {
 
     cec.Open("", "cec.go")
-    var previousState string = ""
+    var previousState int = 5
 
     // Initial Startup tasks
     setupLocalDatabase()
@@ -40,7 +64,8 @@ func main() {
     // Check the TV state every second
     for {
         state := cec.GetDevicePowerStatus(0)
-        if state != previousState{
+        if debug {fmt.Print(state)}
+        if state != previousState {
             go changedState(state)
         }
         previousState = state
